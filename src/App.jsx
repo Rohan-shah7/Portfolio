f4906ede7df849fced870b187360cd35bc78cd8f
Rohan Shah <rohanshah2090@gmail.com>
2026-01-13 12:49:23 +0545

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-orange-900">
+      <h1 className="text-4xl font-bold text-pink-500">
+        Tailwind v4 + React 18 works! 🚀
+      </h1>
+    </div>
+  )
+}
